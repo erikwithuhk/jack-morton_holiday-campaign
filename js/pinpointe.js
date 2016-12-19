8beb68e4588e621013f7195210d3673bce4d5585
erikwithuhk <efjonsson@gmail.com>
2016-12-19 14:50:49 -0500

    add pinpointe tracking script

---
+++ js/pinpointe.js
@@ -0,0 +1,14 @@
+<!-- Pinpointe Tracking Code -->
+<script type="text/javascript">
+var mypinpointe_site_ids = mypinpointe_site_ids || [];
+mypinpointe_site_ids.push(100901581);
+(function() {
+ var s = document.createElement('script');
+ s.type = 'text/javascript';
+ s.async = true;
+ s.src = '//hello.staticstuff.net/w/mypinpointe.js';
+ ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+</script>
+<noscript><p><img alt="Pinpointe Web Analysis" width="1" height="1" src="//win.staticstuff.net/100901581ns.gif" /></p></noscript>
+<!-- End Pinpointe Tracking Code -->
